54d9dc404bdb301cce743ed6a2708caada23ce9c
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-04-08 17:50:28 +0600

    add random_cat_fact class

--- bin/random_cat_fact.dart
+++ bin/random_cat_fact.dart
@@ -1,3 +1,30 @@
-class RandomCatFact {
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+class RandomCatFact {
+  final String fact;
+
+  RandomCatFact({required this.fact});
+
+  static Future<RandomCatFact> fetchRandomCatFact () async {
+    final response = await http.get(Uri.parse('https://catfact.ninja/fact'));
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      return RandomCatFact(fact: json['fact']);
+    } else {
+      throw Exception('Error fetching random cat fact');
+    }
+  }
+
+  void display() {
+    print(
+    '''
+    ======================================
+    $fact
+    ======================================
+    '''
+    );
+  }
 }
