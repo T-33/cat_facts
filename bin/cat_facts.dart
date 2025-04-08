8502381defcf0dfe64593f7ec7c4c670f3c4452f
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-04-08 21:16:26 +0600

    add cat fact menu

--- bin/cat_facts.dart
+++ bin/cat_facts.dart
@@ -4,6 +4,7 @@ import 'random_cat_fact.dart';
 
 class CatFacts {
   Language factsLanguage = Language.english;
+  final List<RandomCatFact> favouriteFacts = [];
 
   static const languagePreferenceCodes = {
     1: Language.english,
@@ -13,14 +14,58 @@ class CatFacts {
 
   CatFacts() {
     factsLanguage = _getLanguagePreference();
-    while (true) {
-      displayRandomCat();
-    }
+    displayRandomCat();
   }
 
   Future<void> displayRandomCat() async {
     var catFact = await RandomCatFact.fetchRandomCatFact();
     catFact.display();
+
+    displayCatMenu();
+    readCatMenuCommand(catFact);
+  }
+
+  void displayCatMenu() {
+    print(
+    '''
+    Enter command:
+    "like" - add current fact to favourites and display next.
+    "next" - display next.
+    "list" - list favourite facts. 
+    "q" - quit. 
+    '''
+    );
+  }
+
+  void readCatMenuCommand(RandomCatFact currentCatFact) {
+    String? userInput = stdin.readLineSync(encoding: utf8)?.trim();
+
+    if (userInput == null || userInput.isEmpty) {
+      print('Invalid command');
+      displayCatMenu();
+    }
+
+    switch (userInput) {
+      case 'like':
+        likeFact(currentCatFact);
+        displayRandomCat();
+      case 'list':
+        listLikedFacts();
+      case 'next':
+        displayRandomCat();
+      case 'quit':
+        return;
+    }
+  }
+
+  void likeFact(RandomCatFact fact) {
+    favouriteFacts.add(fact);
+  }
+
+  void listLikedFacts() {
+    for (final fact in favouriteFacts) {
+      fact.display();
+    }
   }
 
   Language _getLanguagePreference() {
